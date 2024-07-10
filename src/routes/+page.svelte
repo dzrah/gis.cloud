014e238047f27c01f0761f47fef52cefc90e1d1a
dzrah <42093303+dzrah@users.noreply.github.com>
2024-07-10 09:14:09 +0400

    first draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Countdown from './countdown.svelte';
+</script>
+
+<main
+	class="bg-bg-img flex h-screen max-h-screen flex-col justify-start bg-cover bg-no-repeat pl-48 pt-40 before:absolute before:inset-0 before:block before:bg-opacity-40 before:bg-gradient-to-t before:from-[#13C2CC] before:to-[#13c3cc00] before:opacity-50 before:content-['']"
+>
+	<div class="z-20 flex w-[52rem] flex-col">
+		<div class="flex h-auto w-96 items-center justify-center">
+			<img src="/gis_cloud_logo.svg" alt="" />
+		</div>
+		<div>
+			<p class="mt-3 text-5xl font-semibold text-white">Launching gis.cloud soon</p>
+		</div>
+		<div class="mt-24 flex w-[59rem] flex-col">
+			<div class="flex items-center justify-start">
+				<p class="mt-3 text-3xl text-white">
+					Sign up to our newsletter to receive the latest updates
+				</p>
+			</div>
+			<form class="mt-5 flex w-auto flex-row items-center gap-5">
+				<div class="flex">
+					<input
+						class="h-[2.5rem] rounded-[.3rem] placeholder:ml-5"
+						type="text"
+						placeholder="Name"
+						id="name"
+						size="40"
+					/>
+				</div>
+				<div class="flex flex-row">
+					<input
+						class="h-[2.5rem] rounded-[.3rem] rounded-r-[0]"
+						type="text"
+						placeholder="Email address"
+						id="email address"
+						size="40"
+					/>
+					<button
+						class="flex items-center justify-center rounded-md rounded-l-[0] bg-gradient-to-l from-[#126FF0] to-[#13C2CC] px-4 py-2 font-semibold uppercase tracking-wide text-white"
+						>Subscribe</button
+					>
+				</div>
+				<!-- <input class="h-8 rounded-[.3rem]" type="submit" value="submit" size="40" /> -->
+			</form>
+			<Countdown />
+		</div>
+	</div>
+</main>
---
+++ src/routes/countdown.svelte
@@ -0,0 +1,55 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const days = document.getElementById('days');
+		const hours = document.getElementById('hours');
+		const minutes = document.getElementById('minutes');
+		const seconds = document.getElementById('seconds');
+		const days_t = document.getElementById('days-t');
+
+		const launchDate = new Date('July 29 2024 12:00:00');
+
+		function updateCountDown() {
+			const currentTime = new Date();
+			const diff = launchDate - currentTime;
+
+			const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+			const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+			const m = Math.floor(diff / 1000 / 60) % 60;
+			const s = Math.floor(diff / 1000) % 60;
+
+			days.innerHTML = d;
+			hours.innerHTML = h < 10 ? '0' + h : h;
+			minutes.innerHTML = m < 10 ? '0' + m : m;
+			seconds.innerHTML = s < 10 ? '0' + s : s;
+			days_t.innerHTML = d;
+		}
+
+		setInterval(updateCountDown, 1000);
+	});
+</script>
+
+<div class="flex items-center justify-start">
+	<div class="mt-9 flex items-center justify-start gap-2 text-3xl font-semibold text-white">
+		<div class="g flex flex-col items-center justify-center">
+			<div class="flex items-center justify-center gap-2">
+				<h2 id="days">00</h2>
+				<p>:</p>
+			</div>
+			<p class="text-[0.8rem]">Days</p>
+		</div>
+		<div class="flex items-center justify-center gap-2">
+			<h2 id="hours">00</h2>
+			<p>:</p>
+		</div>
+		<div class="flex items-center justify-center gap-2">
+			<h2 id="minutes">00</h2>
+			<p>:</p>
+		</div>
+		<div class="flex items-center justify-center gap-2">
+			<h2 id="seconds">00</h2>
+		</div>
+	</div>
+	<!-- <div class="ml-9"><h2 id="days-t">days</h2></div> -->
+</div>
