5cd0ac926cc106ac716d1a704ae51deea84af4c4
dzrah <42093303+dzrah@users.noreply.github.com>
2024-07-11 12:25:17 +0400

    adding google analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,4 +23,8 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-export { db };
+let analytics;
+if (typeof window !== 'undefined') {
+	analytics = getAnalytics(app);
+}
+export { db, app, analytics };
