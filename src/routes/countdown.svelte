ae3a4401519dd2939fce683888ab3f77826a8a5d
dzrah <42093303+dzrah@users.noreply.github.com>
2024-07-15 14:08:37 +0400

    fixing the error coming from the countdown

--- src/routes/countdown.svelte
+++ src/routes/countdown.svelte
@@ -1,32 +1,30 @@
 <script>
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 
+	let days = '00',
+		hours = '00',
+		minutes = '00',
+		seconds = '00';
+
+	const launchDate = new Date('December 2 2024 12:00:00').getTime();
+
+	function updateCountDown() {
+		const currentTime = new Date().getTime();
+		const diff = launchDate - currentTime;
+
+		days = Math.floor(diff / 1000 / 60 / 60 / 24);
+		hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+		minutes = Math.floor(diff / 1000 / 60) % 60;
+		seconds = Math.floor(diff / 1000) % 60;
+	}
+
+	const interval = setInterval(updateCountDown, 1000);
 	onMount(() => {
-		const days = document.getElementById('days');
-		const hours = document.getElementById('hours');
-		const minutes = document.getElementById('minutes');
-		const seconds = document.getElementById('seconds');
-		const days_t = document.getElementById('days-t');
+		updateCountDown();
+	});
 
-		const launchDate = new Date('December 2 2024 12:00:00');
-
-		function updateCountDown() {
-			const currentTime = new Date();
-			const diff = launchDate - currentTime;
-
-			const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-			const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-			const m = Math.floor(diff / 1000 / 60) % 60;
-			const s = Math.floor(diff / 1000) % 60;
-
-			days.innerHTML = d;
-			hours.innerHTML = h < 10 ? '0' + h : h;
-			minutes.innerHTML = m < 10 ? '0' + m : m;
-			seconds.innerHTML = s < 10 ? '0' + s : s;
-			days_t.innerHTML = d;
-		}
-
-		setInterval(updateCountDown, 1000);
+	onDestroy(() => {
+		clearInterval(interval);
 	});
 </script>
 
@@ -35,7 +33,7 @@
 		<div class="flex-1 flex-col items-center justify-center">
 			<p class="text-[0.8rem]">Days</p>
 			<div class="flex items-center justify-center gap-2">
-				<h2 id="days">00</h2>
+				<h2 id="days">{days}</h2>
 				<p>:</p>
 			</div>
 		</div>
@@ -43,7 +41,7 @@
 		<div class="flex-1 flex-col items-center justify-center">
 			<p class="text-[0.8rem]">Hours</p>
 			<div class="flex items-center justify-center gap-3">
-				<h2 id="hours">00</h2>
+				<h2 id="hours">{hours}</h2>
 				<p>:</p>
 			</div>
 		</div>
@@ -51,7 +49,7 @@
 		<div class="flex-1 flex-col items-center justify-center">
 			<p class="text-[0.8rem]">Minutes</p>
 			<div class="flex items-center justify-center gap-3">
-				<h2 id="minutes">00</h2>
+				<h2 id="minutes">{minutes}</h2>
 				<p>:</p>
 			</div>
 		</div>
@@ -59,7 +57,7 @@
 		<div class="flex-1 flex-col items-center justify-center">
 			<p class="text-[0.8rem]">Seconds</p>
 			<div class="flex items-center justify-center gap-0">
-				<h2 id="seconds">00</h2>
+				<h2 id="seconds">{seconds}</h2>
 			</div>
 		</div>
 	</div>
